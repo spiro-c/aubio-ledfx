8465aaa2ece2641488e16526b9573a1787d18ba3
spiro-c <spiroc@gmail.com>
2025-11-16 23:27:02 +1100

    Link ffmeg and rubberband dynamic

--- vcpkg-triplets/arm64-linux-pic.cmake
+++ vcpkg-triplets/arm64-linux-pic.cmake
@@ -6,8 +6,15 @@ set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 
 set(VCPKG_CMAKE_SYSTEM_NAME Linux)
+set(VCPKG_BUILD_TYPE release)
 
 # Force position-independent code for all static libraries
-# This is critical for FFmpeg and other dependencies when linking into shared libraries
+# This is critical for linking into shared objects (.so files)
 set(VCPKG_C_FLAGS "-fPIC")
 set(VCPKG_CXX_FLAGS "-fPIC")
+if(PORT MATCHES "ffmpeg")
+    set(VCPKG_LIBRARY_LINKAGE dynamic)
+endif()
+if(PORT MATCHES "rubberband")
+    set(VCPKG_LIBRARY_LINKAGE dynamic)
+endif()
